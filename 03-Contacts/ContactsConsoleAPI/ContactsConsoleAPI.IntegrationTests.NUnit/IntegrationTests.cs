889bba9a8c23c72d01760ae0e6e25f1ec711b21c
Georgi Georgiev <63241083+georgi9820@users.noreply.github.com>
2024-02-21 14:39:59 +0200

    update: added new integration tests

--- 03-Contacts/ContactsConsoleAPI/ContactsConsoleAPI.IntegrationTests.NUnit/IntegrationTests.cs
+++ 03-Contacts/ContactsConsoleAPI/ContactsConsoleAPI.IntegrationTests.NUnit/IntegrationTests.cs
@@ -89,121 +89,270 @@ namespace ContactsConsoleAPI.IntegrationTests.NUnit
         public async Task DeleteContactAsync_WithValidULID_ShouldRemoveContactFromDb()
         {
             // Arrange
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            await contactManager.AddAsync(newContact);
 
             // Act
+            await contactManager.DeleteAsync(newContact.Contact_ULID); // Изчакай изпълнението на метода
 
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            var contactInDB = await dbContext.Contacts.FirstOrDefaultAsync(x => x.Contact_ULID == newContact.Contact_ULID);
+            Assert.IsNull(contactInDB);
+
         }
 
-        [Test]
-        public async Task DeleteContactAsync_TryToDeleteWithNullOrWhiteSpaceULID_ShouldThrowException()
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public async Task DeleteContactAsync_TryToDeleteWithNullOrWhiteSpaceULID_ShouldThrowException(string invalidULID)
         {
-            // Arrange
-
-            // Act
-
-            // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() =>  contactManager.DeleteAsync(invalidULID));
         }
 
         [Test]
         public async Task GetAllAsync_WhenContactsExist_ShouldReturnAllContacts()
         {
             // Arrange
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
 
+            var secondNewContact = new Contact()
+            {
+                FirstName = "SecondTestFirstName",
+                LastName = "SecondTestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "2ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933778"
+            };
+
+            await contactManager.AddAsync(newContact);
+            await contactManager.AddAsync(secondNewContact);
             // Act
-
+            var result =  await contactManager.GetAllAsync();
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(result.Count(), Is.EqualTo(2));
+
+            var firstContact = result.First();
+            Assert.That(firstContact.Email, Is.EqualTo(newContact.Email));
         }
 
         [Test]
         public async Task GetAllAsync_WhenNoContactsExist_ShouldThrowKeyNotFoundException()
         {
-            // Arrange
-
-            // Act
-
-            // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.GetAllAsync());
+            Assert.That(ex.Message, Is.EqualTo("No contact found."));
         }
 
         [Test]
         public async Task SearchByFirstNameAsync_WithExistingFirstName_ShouldReturnMatchingContacts()
         {
             // Arrange
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            var secondNewContact = new Contact()
+            {
+                FirstName = "SecondTestFirstName",
+                LastName = "SecondTestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "2ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933778"
+            };
+
+            await contactManager.AddAsync(newContact);
+            await contactManager.AddAsync(secondNewContact);
 
             // Act
-
+            var result = await contactManager.SearchByFirstNameAsync(secondNewContact.FirstName);
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(result.Count(), Is.EqualTo(1));
+            var itemInDB = result.First();
+            Assert.That(itemInDB.LastName, Is.EqualTo(secondNewContact.LastName));
         }
 
         [Test]
         public async Task SearchByFirstNameAsync_WithNonExistingFirstName_ShouldThrowKeyNotFoundException()
         {
-            // Arrange
 
             // Act
-
+            var ex = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.SearchByFirstNameAsync("NO_SUCH_KEY"));
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(ex.Message, Is.EqualTo("No contact found with the given first name."));
         }
 
         [Test]
         public async Task SearchByLastNameAsync_WithExistingLastName_ShouldReturnMatchingContacts()
         {
             // Arrange
-
+            var newContacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    FirstName = "TestFirstName",
+                    LastName = "TestLastName",
+                    Address = "Anything for testing address",
+                    Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                    Email = "test@gmail.com",
+                    Gender = "Male",
+                    Phone = "0889933779"
+                },
+                new Contact()
+                {
+                    FirstName = "SecondTestFirstName",
+                    LastName = "SecondTestLastName",
+                    Address = "Anything for testing address",
+                    Contact_ULID = "2ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                    Email = "test@gmail.com",
+                    Gender = "Male",
+                    Phone = "0889933778"
+                }
+            };
+        
+            foreach (var contact in newContacts)
+            {
+                await contactManager.AddAsync(contact);
+            }
             // Act
-
+            var result = await contactManager.SearchByLastNameAsync(newContacts[0].LastName);
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(result.Count(), Is.EqualTo(1));
+            var itemInDB = result.First();
+            Assert.That(itemInDB.FirstName, Is.EqualTo(newContacts[0].FirstName));
         }
 
         [Test]
         public async Task SearchByLastNameAsync_WithNonExistingLastName_ShouldThrowKeyNotFoundException()
         {
-            // Arrange
-
             // Act
-
+            var ex = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.SearchByLastNameAsync("NO_SUCH_LAST_NAME"));
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(ex.Message, Is.EqualTo("No contact found with the given last name."));
         }
 
         [Test]
         public async Task GetSpecificAsync_WithValidULID_ShouldReturnContact()
         {
             // Arrange
+            var newContacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    FirstName = "TestFirstName",
+                    LastName = "TestLastName",
+                    Address = "Anything for testing address",
+                    Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                    Email = "test@gmail.com",
+                    Gender = "Male",
+                    Phone = "0889933779"
+                },
+                new Contact()
+                {
+                    FirstName = "SecondTestFirstName",
+                    LastName = "SecondTestLastName",
+                    Address = "Anything for testing address",
+                    Contact_ULID = "2ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                    Email = "test@gmail.com",
+                    Gender = "Male",
+                    Phone = "0889933778"
+                }
+            };
+
+            foreach (var contact in newContacts)
+            {
+                await contactManager.AddAsync(contact);
+            }
 
             // Act
-
+            var result = await contactManager.GetSpecificAsync(newContacts[1].Contact_ULID);
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.NotNull(result);
+            Assert.That(result.FirstName, Is.EqualTo(newContacts[1].FirstName));
         }
 
         [Test]
         public async Task GetSpecificAsync_WithInvalidULID_ShouldThrowKeyNotFoundException()
         {
-            // Arrange
-
             // Act
-
+            const string invalidULID = "NON_VALID_ULID";
+            var ex = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.GetSpecificAsync(invalidULID));
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(ex.Message, Is.EqualTo($"No contact found with ULID: {invalidULID}"));
         }
 
         [Test]
         public async Task UpdateAsync_WithValidContact_ShouldUpdateContact()
         {
             // Arrange
+            var newContacts = new List<Contact>()
+            {
+                new Contact()
+                {
+                    FirstName = "TestFirstName",
+                    LastName = "TestLastName",
+                    Address = "Anything for testing address",
+                    Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                    Email = "test@gmail.com",
+                    Gender = "Male",
+                    Phone = "0889933779"
+                },
+                new Contact()
+                {
+                    FirstName = "SecondTestFirstName",
+                    LastName = "SecondTestLastName",
+                    Address = "Anything for testing address",
+                    Contact_ULID = "2ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                    Email = "test@gmail.com",
+                    Gender = "Male",
+                    Phone = "0889933778"
+                }
+            };
+
+            foreach (var contact in newContacts)
+            {
+                await contactManager.AddAsync(contact);
+            }
+
+            var modifiedContact = newContacts[0];
+            modifiedContact.Gender = "Female";
 
             // Act
-
+            await contactManager.UpdateAsync(modifiedContact);
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            var itemInDB = await dbContext.Contacts.FirstOrDefaultAsync(x => x.Contact_ULID == modifiedContact.Contact_ULID);
+            Assert.IsNotNull(itemInDB);
+            Assert.That(itemInDB.Gender, Is.EqualTo(modifiedContact.Gender));
         }
 
         [Test]
@@ -212,9 +361,9 @@ namespace ContactsConsoleAPI.IntegrationTests.NUnit
             // Arrange
 
             // Act
-
+            var ex = Assert.ThrowsAsync<ValidationException>(() => contactManager.UpdateAsync(new Contact()));
             // Assert
-            Assert.Inconclusive("Test not implemented yet.");
+            Assert.That(ex.Message, Is.EqualTo("Invalid contact!"));
         }
     }
 }
